656f451ecbefe58784dd85a350dbbc74beb4f44f
Jose Orellana <jose.orellana@kantaribopemedia.com>
2023-01-16 16:15:38 -0300

    Hello world y variables

---
+++ Python/00_Hello.py
@@ -0,0 +1,26 @@
+print("Hello Python")
+
+# esto es un comentario
+
+"""
+esto es u comentario
+en varias 
+lineas
+"""
+
+'''
+esto es u comentario
+en varias 
+lineas
+'''
+
+#consultar el tipo de datos
+print(type("jose"))  # tipo 'str'
+print(type(10)) # tipo int
+print(type(3.5)) #tipo float
+print(type(3.5)) #tipo float
+print(type(1+3j)) # tipo complex
+
+print(type([1,2,3])) #list
+
+
---
+++ Python/01_Variables.py
@@ -0,0 +1,14 @@
+#variables
+
+my_string_variable = "my string variable"
+print(my_string_variable)
+
+
+my_int_variable = 10
+print(my_int_variable)
+
+
+my_bool_variable = True
+print(my_bool_variable)
+
+print(my_string_variable, my_int_variable)
