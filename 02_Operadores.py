b9311ad20e480d078cbc1e9a965b915799d169d5
Jose Orellana <jose.orellana@kantaribopemedia.com>
2023-01-17 15:52:44 -0300

    Operadores

---
+++ 02_Operadores.py
@@ -0,0 +1,53 @@
+### operadores ###
+
+#aritmeticos
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+
+print(3 % 4)
+print(3 // 4)
+print(3 ** 4) #exponente
+
+
+print("hola" + " p")
+print("hola" + str(5))
+print("hola" * 4)
+
+print("hola" * 2**3)
+
+#comparativos
+
+print(3 > 4)#false
+print(3 < 4)#true
+print(3 >= 4)#false
+print(3 <= 4)#true
+print(3 == 4)#false
+print(3 != 4)#true
+print(3 < 4==4)#true
+
+#valida por orden alfabetico por ASCII
+print("hola" > "p")#
+print("hola" < "p")#
+print("hola" >= "p")#
+print("hola" <= "p")#
+print("hola" == "p")#
+print("hola" != "p")#
+
+
+
+#logicos
+
+### logica booleano
+# false and false =false
+# false and true = false
+# false or false = false
+
+print (3 > 4 and "hola" > "python")
+print (3 > 4 or "hola" > "python")
+print (3 < 4 and "hola" < "python")
+print (3 < 4 or "hola" < "python")
+print (3 < 4 or ("hola" < "python" and 4==4))
+
+print(not 3 > 4)
